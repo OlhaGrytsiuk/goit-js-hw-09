4f383dfa1b8b6024350dac43929413a7f897a03a
OlhaGrytsiuk <117648979+OlhaGrytsiuk@users.noreply.github.com>
2023-04-23 11:51:54 +0000

    Deploying to gh-pages from @ OlhaGrytsiuk/goit-js-hw-09@46cdccc43b13a467e0caf41b3da6d1cc0855fb03 🚀

---
+++ 01-color-switcher.055d25db.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r=null;t.addEventListener("click",(function(){r=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.055d25db.js.map
